432c3bd9c1cd8ad95bfc5cf5a9f0b6eb03d35255
itvincent-git <zhongyongsheng@yy.com>
2019-12-26 11:14:01 +0800

    增加注释

--- lib/src/main/java/net/androidx/gestureanimate/CollapsibleToolbar.kt
+++ lib/src/main/java/net/androidx/gestureanimate/CollapsibleToolbar.kt
@@ -4,7 +4,14 @@ import android.content.Context
 import android.support.constraint.motion.MotionLayout
 import android.support.design.widget.AppBarLayout
 import android.util.AttributeSet
+import net.slog.SLoggerFactory
 
+/**
+ * 将AppBarLayout滚动事件，触发为MotionLayout.progress的变化
+ * AppBarLayout滑动向上，则verticalOffset为负数并且越来越小，progress则由0向1变化
+ * @author zhongyongsheng
+ */
+private val log = SLoggerFactory.getLogger("CollapsibleToolbar")
 class CollapsibleToolbar @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : MotionLayout(context, attrs, defStyleAttr), AppBarLayout.OnOffsetChangedListener {
