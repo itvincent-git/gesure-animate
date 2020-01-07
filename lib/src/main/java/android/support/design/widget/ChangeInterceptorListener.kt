b7bd1bbc74385e4b4765443658b02c120ee031d7
itvincent-git <zhongyongsheng@yy.com>
2020-01-07 18:36:59 +0800

    换成recyclerview测试

---
+++ lib/src/main/java/android/support/design/widget/ChangeInterceptorListener.kt
@@ -0,0 +1,37 @@
+package android.support.design.widget
+
+import android.content.Context
+import android.view.animation.DecelerateInterpolator
+import android.view.animation.Interpolator
+import net.slog.SLoggerFactory
+import kotlin.math.absoluteValue
+
+/**
+ * 实现往下拉的阻尼效果
+ */
+class ChangeInterceptorListener(val context: Context, val layoutParam: AppBarLayout.LayoutParams) :
+    AppBarLayout.OnOffsetChangedListener {
+    private var lastOffset = 0
+    val interpolator: Interpolator =
+        DecelerateInterpolator(1.0f)
+
+    override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+        if (lastOffset != verticalOffset) {
+            lastOffset = verticalOffset
+            val scrollRange = appBarLayout.totalScrollRange
+            if (verticalOffset.absoluteValue == scrollRange) {
+                // appbar最小化
+                log.debug("onAppBarMinimize")
+                layoutParam.scrollInterpolator = interpolator
+            } else if (verticalOffset.absoluteValue == 0) {
+                // appbar最大化
+                log.debug("onAppBarMaximize")
+                layoutParam.scrollInterpolator = null
+            }
+        }
+    }
+
+    companion object {
+        private val log = SLoggerFactory.getLogger("ChangeInterceptorListener")
+    }
+}
--- sample/src/main/java/net/androidx/gestureanimate/sample/WechatLikeActivity.kt
+++ sample/src/main/java/net/androidx/gestureanimate/sample/WechatLikeActivity.kt
@@ -1,20 +1,23 @@
 package net.androidx.gestureanimate.sample
 
-import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.AppBarLayout
+import android.support.design.widget.ChangeInterceptorListener
 import android.support.v7.app.AppCompatActivity
-import android.view.animation.AnimationUtils
-import android.view.animation.DecelerateInterpolator
-import android.view.animation.Interpolator
+import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_wechat_like.app_bar
-import kotlinx.android.synthetic.main.layout_content_scrolling.large_tv
+import kotlinx.android.synthetic.main.layout_content_scrolling.recycler_view
 import kotlinx.android.synthetic.main.layout_coordinator_header.motionLayout
 import net.androidx.gestureanimate.util.createRandomString
+import net.androidx.kangga.sample.MyAdapter
 import net.slog.SLoggerFactory
-import kotlin.math.absoluteValue
 
 class WechatLikeActivity : AppCompatActivity() {
+    private val myAdapter = MyAdapter(mutableListOf()).apply {
+        for (i in 0 until 30) {
+            myDataset.add(createRandomString(10))
+        }
+    }
 
     companion object {
         private val log = SLoggerFactory.getLogger("WechatLikeActivity")
@@ -24,41 +27,18 @@ class WechatLikeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wechat_like)
 
-        large_tv.text = createRandomString(1024 * 8)
+        //large_tv.text = createRandomString(1024 * 8)
+        recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@WechatLikeActivity)
+            adapter = myAdapter
+        }
 
         val headerLayoutParam = motionLayout.layoutParams as AppBarLayout.LayoutParams
 //        headerLayoutParam.scrollFlags = AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL or AppBarLayout.LayoutParams
 //            .SCROLL_FLAG_EXIT_UNTIL_COLLAPSED or AppBarLayout.LayoutParams.SCROLL_FLAG_SNAP
 
-        app_bar.addOnOffsetChangedListener(ChangeInterceptorListener(this, headerLayoutParam))
+        app_bar.addOnOffsetChangedListener(
+            ChangeInterceptorListener(this, headerLayoutParam))
     }
 }
 
-/**
- * 实现往下拉的阻尼效果
- */
-class ChangeInterceptorListener(val context: Context, val layoutParam: AppBarLayout.LayoutParams) :
-    AppBarLayout.OnOffsetChangedListener {
-    private var lastOffset = 0
-    val interpolator: Interpolator = DecelerateInterpolator(1.8f)
-
-    override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
-        if (lastOffset != verticalOffset) {
-            lastOffset = verticalOffset
-            val scrollRange = appBarLayout.totalScrollRange
-            if (verticalOffset.absoluteValue == scrollRange) {
-                // appbar最小化
-                log.debug("onAppBarMinimize")
-                layoutParam.scrollInterpolator = interpolator
-            } else if (verticalOffset.absoluteValue == 0) {
-                // appbar最大化
-                log.debug("onAppBarMaximize")
-                layoutParam.scrollInterpolator = null
-            }
-        }
-    }
-
-    companion object {
-        private val log = SLoggerFactory.getLogger("ChangeInterceptorListener")
-    }
-}
