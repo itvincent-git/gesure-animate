64d5e9fc4377ef7225a4eabde17f22a9ae35540f
itvincent-git <zhongyongsheng@yy.com>
2020-01-09 16:11:46 +0800

    测试效果

--- lib/src/main/java/android/support/design/widget/MoreSlowInterpolator.kt
+++ lib/src/main/java/android/support/design/widget/MoreSlowInterpolator.kt
@@ -5,15 +5,18 @@ import net.slog.SLoggerFactory
 import kotlin.math.pow
 
 /**
- * 测试用
+ * 实现wechat缓动曲线的插值器
  * Created by zhongyongsheng on 2020-01-08.
  */
 class MoreSlowInterpolator : Interpolator {
-    //(1 - (x-1)^2 )* 0.5
     override fun getInterpolation(input: Float): Float {
 //        val ret = (1.0f - (input - 1.0f).toDouble().pow(2.0)).toFloat()
-        val ret = 1.0f - (1.0f - input.toDouble().pow(2.0)).pow(0.5).toFloat()
-        log.debug("getInterpolation $input > $ret")
+//        val ret = 1.0f - (1.0f - input.toDouble().pow(2.0)).pow(0.5).toFloat()
+        var ret = input
+        if (input < 0.9f) {
+            ret = ((1.0f - (1.0f - input).toDouble().pow(3.0))).toFloat() * 0.9009009f
+        }
+        //log.debug("getInterpolation $input > $ret")
         return ret
     }
 
