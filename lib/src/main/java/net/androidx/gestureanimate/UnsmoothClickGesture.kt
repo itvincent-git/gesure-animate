e18cfc52bb13050545f4b4d11c21a52b6145847b
itvincent-git <zhongyongsheng@yy.com>
2020-03-11 11:53:31 +0800

    根据最初的位置来判断是否能拖动

--- lib/src/main/java/net/androidx/gestureanimate/UnsmoothClickGesture.kt
+++ lib/src/main/java/net/androidx/gestureanimate/UnsmoothClickGesture.kt
@@ -1,12 +1,10 @@
 package net.androidx.gestureanimate
 
 import android.content.Context
+import android.util.SparseArray
 import android.view.MotionEvent
-import android.view.VelocityTracker
-import android.view.ViewConfiguration
+import net.androidx.gestureanimate.util.ScrollConfiguration
 import net.slog.SLoggerFactory
-import kotlin.math.pow
-import kotlin.math.sqrt
 
 /**
  * 提供不能滑动的点击事件。当Down发生时，如果有距离远而且速度快的MOVE，则不触发click事件
@@ -21,52 +19,72 @@ class UnsmoothClickGesture constructor(
         private val log = SLoggerFactory.getLogger("UnsmoothClickGesture")
     }
 
-    private var velocityTracker: VelocityTracker? = null
-    private val minimumFlingVelocity = ViewConfiguration.getMinimumFlingVelocity().toFloat()
-    private val maximumFlingVelocity = ViewConfiguration.getMaximumFlingVelocity().toFloat()
+    private var activePointerId = MotionEvent.INVALID_POINTER_ID
+    private val initialMotionX = SparseArray<Float>() //记录点击开始时的X
+    private val initialMotionY = SparseArray<Float>() //记录点击开始时的Y
+    private var lastTouchX = 0f
+    private var lastTouchY = 0f
+    private val scrollConfiguration = ScrollConfiguration(context) // 从系统及通过算法处理后的滚动参数
+
+    private fun initMotion(event: MotionEvent) {
+        // Remember where we started (for dragging)
+        val pointerIndex = event.actionIndex
+        lastTouchX = event.getX(pointerIndex)
+        lastTouchY = event.getY(pointerIndex)
+        activePointerId = event.getPointerId(pointerIndex)
+        initialMotionX.put(activePointerId, lastTouchX)
+        initialMotionY.put(activePointerId, lastTouchY)
+    }
+
+    private fun resetMotion(event: MotionEvent, newPointerIndex: Int) {
+        lastTouchX = event.getX(newPointerIndex)
+        lastTouchY = event.getY(newPointerIndex)
+        activePointerId = event.getPointerId(newPointerIndex)
+
+        initialMotionX.clear()
+        initialMotionY.clear()
+    }
 
     /**
      * 调用点击事件的处理逻辑
      */
     fun onTouchEvent(event: MotionEvent): Boolean {
-        log.debug("onTouchEvent $event")
+        //log.debug("onTouchEvent $event")
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
-                // Reset the velocity tracker back to its initial state.
-                velocityTracker?.clear()
-                // If necessary retrieve a new VelocityTracker object to watch the
-                // velocity of a motion.
-                velocityTracker = velocityTracker ?: VelocityTracker.obtain()
-                // Add a user's movement to the tracker.
-                velocityTracker?.addMovement(event)
-            }
-            MotionEvent.ACTION_MOVE -> {
-
-                velocityTracker?.apply {
-                    addMovement(event)
-                    // When you want to determine the velocity, call
-                    // computeCurrentVelocity(). Then call getXVelocity()
-                    // and getYVelocity() to retrieve the velocity for each pointer ID.
-                    computeCurrentVelocity(1000, maximumFlingVelocity)
-                }
+                initMotion(event)
             }
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-
-                velocityTracker?.apply {
-                    computeCurrentVelocity(1000, maximumFlingVelocity)
-                    val pointerId = event.getPointerId(event.actionIndex)
-                    val velocity = sqrt(getXVelocity(pointerId).toDouble().pow(2.0) + getYVelocity(
-                        pointerId).toDouble().pow(2.0))
-                    //如果速度超过vel或者距离超过dis则不分发点击
-
-                    if (velocity < minimumFlingVelocity) {
-                        callback.onUnsmoothClick(event)
+                // 移动的距离
+                val (x: Float, y: Float) =
+                    event.findPointerIndex(activePointerId).let { pointerIndex ->
+                        // Calculate the distance moved
+                        event.getX(pointerIndex) to
+                            event.getY(pointerIndex)
                     }
-                }
 
-                // Return a VelocityTracker object back to be re-used by others.
-                velocityTracker?.recycle()
-                velocityTracker = null
+                //从开始触摸到现在总移动距离
+                val dx = x - initialMotionX[activePointerId]
+                val dy = y - initialMotionY[activePointerId]
+
+                val checkSlop = dx * dx + dy * dy <= (scrollConfiguration.touchSlop * scrollConfiguration.touchSlop)
+                    .toFloat()
+                if (checkSlop) {
+                    callback.onUnsmoothClick(event)
+                }
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+
+                event.actionIndex.also { pointerIndex ->
+                    event.getPointerId(pointerIndex)
+                        .takeIf { it == activePointerId }
+                        ?.run {
+                            // This was our active pointer going up. Choose a new
+                            // active pointer and adjust accordingly.
+                            val newPointerIndex = if (pointerIndex == 0) 1 else 0
+                            resetMotion(event, newPointerIndex)
+                        }
+                }
             }
         }
         return true
